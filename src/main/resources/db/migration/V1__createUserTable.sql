5b8d3f52a2dcf71becea3fc9251ebb716d40e77c
Borba <gabriel.lewis@acad.pucrs.bbr>
2021-11-03 15:31:43 -0300

    Create user migration

--- src/main/resources/db/migration/V1__createUserTable.sql
+++ src/main/resources/db/migration/V1__createUserTable.sql
@@ -1,7 +1,9 @@
-CREATE TABLE user (
+CREATE TABLE if not exists users (
 	id serial not null primary key,
 	username varchar(255) not null,
 	token varchar(255) not null,
 	email varchar(255) not null,
 	password varchar(255) not null
 );
+
+
